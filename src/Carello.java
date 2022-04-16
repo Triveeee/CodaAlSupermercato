fcbf2015c030f4f8b20b648228772a2d12cfacc6
Triveeee <riccardo,trivellato2004gmail.com>
2022-04-16 16:59:58 +0200

    Nuovo progetto

---
+++ src/Carello.java
@@ -0,0 +1,15 @@
+public class Carello extends Thread {
+
+    private int numero;
+    private String nome;
+
+    public Carello(String nome){
+        numero = 0;
+        this.nome = nome;
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+    }
+}
